ec9d549e9df1f04f9de6e2173a763e6abc18fc81
ptgame75-collab <ptgame75@gmail.com>
2026-01-17 23:14:21 +0545

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// sw.js
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-    // सिधै नेटवर्कबाट तान्ने, केही पनि सेभ नगर्ने
-    event.respondWith(fetch(event.request));
-});
