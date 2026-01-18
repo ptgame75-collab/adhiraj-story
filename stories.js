f1fecf321dcf547cf0b15f43416d80e30e2bc400
ptgame75-collab <ptgame75@gmail.com>
2026-01-18 08:45:14 +0545

    Create stories.js

---
+++ stories.js
@@ -0,0 +1,10 @@
+const hindiStories = [
+    { id: 'h1', title: "पत्थर का दिल", content: "कथाको पूरा विवरण यहाँ लेख्नुहोस्...", moral: "करुणा" },
+    { id: 'h2', title: "स्वार्थी भाई", content: "यो अर्को कथा हो...", moral: "प्रेम" }
+];
+
+const nepaliStories = [
+    { id: 'n1', title: "दिदीको समर्पण", content: "नेपाली कथाको विवरण यहाँ...", moral: "त्याग" },
+    { id: 'n2', title: "अधुरो प्रतिज्ञा", content: "अर्को नेपाली कथा...", moral: "सत्य" }
+];
+
